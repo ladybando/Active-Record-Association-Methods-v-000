3e7eae0771315eb4542435802594a407b16f732b
Ayana Bando <ayanaflax@yahoo.com>
2018-09-17 14:21:14 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.find(1,2,3)
+    self.artistsfind(1,2,3)
   end
 end
